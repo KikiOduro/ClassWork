b9a6e129001a285be206a21424806c91105268d9
Akua Oduro <akua.oduro@ashesi.edu.gh>
2025-09-25 09:10:14 +0000

    Add files via upload

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -1,25 +1,43 @@
 import java.util.Scanner;
 
-pulic class ComputeBMI{
+public class ComputeBMI {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double weight;
-        final float POUND = 0.45359237f;
-        final float KILO = 0.0254f;
-        double height;
+        char letter;
+        // double weight;
+        // final float POUND = 0.45359237f;
+        // final float KILO = 0.0254f;
+        // double height;
 
-        System.out.println("What is your height");
-        height = input.nextDouble();
+        // System.out.println("What is your height");
+        // height = input.nextDouble();
 
-        System.out.println("what is your weight");
-        weight =input.nextDouble();
+        // System.out.println("what is your weight");
+        // weight =input.nextDouble();
 
-        double newHeight= height * POUND;
-        double newWeight = weight*KILO;
+        // double newHeight= height * KILO;
+        // double newWeight = weight*POUND;
 
-        double BMI=newWeight/(newHeight *newHeight);
-        System.out.println(BMI);
-        
-        
+        // double BMI=newWeight/(newHeight *newHeight);
+        // System.out.println(BMI);
+
+        System.out.println("Enter a letter of your choice: ");
+        letter = input.next().charAt(1);
+
+        int choice = switch (letter) {
+            case 'a', 'b', 'c' -> 2;
+            case 'd', 'e', 'f' -> 3;
+            case 'g', 'h', 'i' -> 4;
+            case 'j', 'k', 'l' -> 5;
+            case 'm', 'n', 'o' -> 6;
+            case 'p', 'q', 'r', 's' -> 7;
+            case 't', 'u', 'v' -> 8;
+            case 'w', 'x', 'y', 'z' -> 9;
+            default -> 0;
+        };
+
+        System.out.println("This is the corresponding number" + choice);
+
+        input.close();
     }
 }
